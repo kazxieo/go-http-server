a621831f568e2d9cad15e6369c8fb56e8b6c03ac
kazxieo <astris3412@gmail.com>
2025-04-26 13:03:51 +0530

    Implement a custom http.Server

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/kazxieo/go-http-server/handlers"
 )
 
+const Addr = ":8080" // Should I name this to port? Idk
+
 func main() {
-	panic(http.ListenAndServe(":8080", nil))
+
+	server := http.Server{
+		Addr:    Addr,
+		Handler: &handlers.Handler{},
+	}
+
+	log.Fatalln(server.ListenAndServe())
 }
