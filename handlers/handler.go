f6b12435c3948eb9003dce964953cf092acf0d10
kazxieo <astris3412@gmail.com>
2025-04-26 13:11:36 +0530

    Implement the ServeHTTP method and home handler

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,7 +1,24 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Handler struct{}
 
-func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+
+	case "/":
+		h.home(w, r)
+
+	default:
+		http.Error(w, "Invalid path", http.StatusNotFound)
+	}
+}
+
+func (h *Handler) home(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hey there! This is the home page!\n")
+}
