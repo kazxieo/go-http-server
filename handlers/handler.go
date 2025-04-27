717e9ed6a89d6a804ca9260fb861bb2339bca623
kazxieo <astris3412@gmail.com>
2025-04-27 21:55:08 +0530

    Implement a handler for fetching users

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/kazxieo/go-http-server/database"
 )
 
 type Handler struct{}
@@ -14,6 +17,9 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "/":
 		h.home(w, r)
 
+	case "/users":
+		h.getUsers(w, r)
+
 	default:
 		http.Error(w, "Invalid path", http.StatusNotFound)
 	}
@@ -21,4 +27,12 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) home(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Hey there! This is the home page!\n")
+}
+
+func (h *Handler) getUsers(w http.ResponseWriter, _ *http.Request) {
+
+	if err := json.NewEncoder(w).Encode(database.SampleDatabase); err != nil {
+		http.Error(w, fmt.Sprintf("Error processing JSON: %v", err), http.StatusInternalServerError)
+		return
+	}
 }
