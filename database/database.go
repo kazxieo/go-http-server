337338d47cfb8398778b24ec61a26c8c7506508d
kazxieo <astris3412@gmail.com>
2025-04-28 10:11:56 +0530

    Add a sample password database

--- database/database.go
+++ database/database.go
@@ -7,3 +7,8 @@ type UserDetails struct {
 }
 
 var SampleDatabase = make(map[string]UserDetails)
+
+// In a real life environment, this wouldn't be hardcoded like this
+var PasswordDatabase = map[string]string{
+	"kaz": "kaz@3412",
+}
